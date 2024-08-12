f03ca58c763f75a7912ca822fe30d2628e7d91db
alexangc <alexis.miranda@hotmail.fr>
2024-08-12 16:43:07 +0200

    Finish config for simple sending and receiving

--- api/publish.ts
+++ api/publish.ts
@@ -1,9 +1,26 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Client } from "@upstash/qstash";
+import { maybeLoadEnv } from "../utils/env.js";
+
+maybeLoadEnv();
+
+const client = new Client({ token: process.env.QSTASH_TOKEN as string });
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   if (req.method !== "GET") {
     return res.status(400).send("GET only");
   }
+
+  const url = `${process.env.API_BASE_URL}/api/receive`;
+  console.log("Publishing to", url);
+
+  const maybeRes = await client.publishJSON({
+    url,
+    body: { hello: "world" },
+    // headers: { "my-header": "my-value" },
+  });
+
+  console.log("Publishing result:", maybeRes);
 
   return res.json({ message: "publish.ts" });
 }
--- api/receive.ts
+++ api/receive.ts
@@ -1,10 +1,15 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { maybeLoadEnv } from "../utils/env.js";
 // import { waitUntil } from '@vercel/functions';
+
+maybeLoadEnv();
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   if (req.method !== "POST") {
     return res.status(400).send("POST only");
   }
+
+  console.log("Received messag", req.body);
 
   return res.json({ message: "receive.ts" });
 }
